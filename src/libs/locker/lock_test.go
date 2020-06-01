a7d9516a1ebf8fe0c6a790d2ac6f205d945950c9
Mike Mackintosh <m@zyp.io>
2020-06-01 00:45:33 -0700

    fix test

--- src/libs/locker/lock_test.go
+++ src/libs/locker/lock_test.go
@@ -1,10 +1,31 @@
 package locker
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestWriteWithLock(t *testing.T) {
-	filename := "/tmp/test"
-	err := WriteWithLock(filename, []byte("test"))
+	path := "/tmp/test"
+
+	// check if file exists
+	var _, err = os.Stat(path)
+
+	// create file if not exists
+	if os.IsNotExist(err) {
+		var file, err = os.Create(path)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer file.Close()
+	}
+
+	err = WriteWithLock(path, []byte("test"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = os.Remove(path)
 	if err != nil {
 		t.Fatal(err)
 	}
