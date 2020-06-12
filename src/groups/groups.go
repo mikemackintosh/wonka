25e358948d45ed5277202281d6ce1f7c6380c8dc
Mike Mackintosh <mikemackintosh@users.noreply.github.com>
2020-06-12 00:33:15 -0700

    Update groups.go

--- src/groups/groups.go
+++ src/groups/groups.go
@@ -115,7 +115,7 @@ func Marshal(in Entries) ([]byte, error) {
 	}
 
 	// Join the slices by new lines.
-	return []byte(strings.Join(out, "\n")), nil
+	return []byte(strings.Join(out, "\n") + "\n"), nil
 }
 
 // Save will take in entries.
